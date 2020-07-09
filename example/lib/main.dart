697e5535a78336f0109d0a14671ab952a91cecad
Flonger <xue@flonger.com>
2020-07-09 10:46:04 +0800

    change function

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      platformVersion = await PaytmFlutter.goToPaytm;
+      platformVersion = await PaytmFlutter.goToPaytmWithParams({'aaa':'111'});
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
--- lib/paytm_flutter.dart
+++ lib/paytm_flutter.dart
@@ -6,8 +6,13 @@ class PaytmFlutter {
   static const MethodChannel _channel =
       const MethodChannel('paytm_flutter');
 
+  static Future<String> goToPaytmWithParams(Map map) async{
+    final String version = await _channel.invokeMethod('goToPaytm',map);
+    return version;
+  }
+
   static Future<String> get goToPaytm async {
-    final String version = await _channel.invokeMethod('goToPaytm',{'aaa':111});
+    final String version = await _channel.invokeMethod('goToPaytm');
     return version;
   }
 }
